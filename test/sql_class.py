36eba5caa8abd09b5fa801b4f456ad5cd25197c9
1521970772@qq.com <1521970772@qq.com>
2018-01-19 15:09:30 +0800

    2

--- test/sql_class.py
+++ test/sql_class.py
@@ -3,18 +3,25 @@ import pymysql
 class mySql():
     def __init__(self):
         #创建连接
+
         self.con=pymysql.connect(host='192.168.0.241',port=3306,user='root',password='root',db='zfy-source')
-        self.curser=self.con.cursor()
+        self.curser = self.con.cursor()
+
     def exe_sql(self,sql):
-        self.curser.execute(sql)
+
+        try:
+            self.curser.execute(sql)
+        except Exception as a:
+            self.con.rollback() #执行失败就回滚
+            print("执行失败：%s"%a)
+        self.curser.close()
         self.con.commit()
     def close_con(self):
-        self.curser.close()
-        self.con.close()
+        #关闭数据库连接
+        try:
+            self.con.close()
+        except Exception as a:
+            print("数据库关闭异常： %s"%a)
 
-if __name__ == '__main__':
-    a=mySql()
-    sql='insert into zfy_device values(2,"asff","000123456789","1","mysql","","1","5/24/2017","0","3","5","0","2018-01-18 17:25:31","1029","admin","0","105","105")'
-    a.exe_sql(sql)
-    a.close_con()
+
 
